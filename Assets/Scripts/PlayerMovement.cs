b5e52429e830d163ad308ccd46201fc1e922dcd7
maxarnheiter <maxarnheiter@gmail.com>
2015-02-18 13:40:40 -0500

    Initial Progress
    Background and player ship animation/movement.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerMovement : MonoBehaviour 
+{
+
+	Animator animator;
+	
+	bool moving;
+	float speed;
+	
+	float speedAdjust = 4f;
+	
+	void Start () 
+	{
+		animator = gameObject.GetComponent<Animator>();
+	}
+	
+	void Update () 
+	{
+		speed = Input.GetAxis("Horizontal");
+		
+		animator.SetFloat("speed", speed);
+		
+		float change = speed * Time.deltaTime * speedAdjust;
+		
+		
+		transform.position = new Vector3(transform.position.x + change, transform.position.y, transform.position.z);
+	}
+}
