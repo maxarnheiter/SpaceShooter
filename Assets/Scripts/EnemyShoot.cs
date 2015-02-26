6f9eb9671cd728f56e62e10bcfa90b76664e4381
maxarnheiter <maxarnheiter@gmail.com>
2015-02-25 22:04:38 -0500

    14. Enemy Shot Behavior
    Got almost all of the shot behavior done with the exception of the wave
    shot. Need to find a way to represent half a circle in shots.

---
+++ Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,201 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShoot : MonoBehaviour 
+{
+
+    public EnemyShootBehavior shootBehavior;
+    public float attackInterval;
+    public float bombardXRange;
+    public float clusterSize;
+    public float clusterVariation;
+    public float clusterMissVariation;
+    public float targetMissVariation;
+    public float waveCount;
+
+    public GameObject smallShot;
+    public GameObject mediumShot;
+    public GameObject largeShot;
+
+    public enum EnemyShootBehavior
+    {
+        BlindFire,
+        TargetFire,
+        Bombard,
+        ClusterFire, 
+        WaveBarrage,
+    }
+
+    enum ShotType
+    {
+        Small, 
+        Medium, 
+        Large
+    }
+
+    bool isVisible;
+    GameObject player;
+
+    float lastAttackTime;
+
+	void Start () 
+    {
+       
+	}
+	
+
+	void Update () 
+    {
+	    if(isVisible)
+        {
+            FindPlayer();
+            AttackPlayer();
+        }
+	}
+
+    void OnBecameVisible()
+    {
+        isVisible = true;
+    }
+
+    void OnBecameInvisible()
+    {
+        isVisible = false;
+    }
+
+    void FindPlayer()
+    {
+        if(player == null)
+        {
+            player = GameObject.Find("Player Ship");
+        }
+    }
+
+    void DoShot(ShotType shotType, Vector3 startPosition, Vector3 targetPosition, float speedBoost)
+    {
+        GameObject newShot = null;
+        Shot shot;
+
+        if (shotType == ShotType.Small)
+            newShot = GameObject.Instantiate(smallShot, startPosition, Quaternion.identity) as GameObject;
+
+        if (shotType == ShotType.Medium)
+            newShot = GameObject.Instantiate(mediumShot, startPosition, Quaternion.identity) as GameObject;
+
+        if(shotType == ShotType.Large)
+            newShot = GameObject.Instantiate(largeShot, startPosition, Quaternion.identity) as GameObject;
+
+        if (newShot != null)
+        {
+            shot = newShot.GetComponent<Shot>();
+
+            if (speedBoost > 1f)
+                shot.speed *= speedBoost;
+
+            shot.Shoot(targetPosition);
+        }
+    }
+
+    void AttackPlayer()
+    {
+        if (!MinimumTimeElapsed())
+            return;
+
+        switch(shootBehavior)
+        {
+            case EnemyShootBehavior.BlindFire:
+                BlindFire();
+                break;
+            case EnemyShootBehavior.Bombard:
+                Bombard();
+                break;
+            case EnemyShootBehavior.ClusterFire:
+                ClusterFire();
+                break;
+            case EnemyShootBehavior.TargetFire:
+                TargetFire();
+                break;
+            case EnemyShootBehavior.WaveBarrage:
+                WaveBarrage();
+                break;
+        }
+
+        lastAttackTime = Time.realtimeSinceStartup;
+    }
+
+    void BlindFire()
+    {
+        DoShot(ShotType.Small, transform.position, Vector3.down, 0f);
+    }
+
+    void Bombard()
+    {
+        int randShot = Random.Range(0, 2);
+        float xChange = Random.Range((bombardXRange * -1f), bombardXRange);
+        var startPos = new Vector3(transform.position.x + xChange, transform.position.y, transform.position.z);
+
+        if(randShot == 0)
+            DoShot(ShotType.Small, startPos, Vector3.down, 0f);
+        
+        if(randShot == 1)
+            DoShot(ShotType.Medium, startPos, Vector3.down, 0f);
+    }
+
+    void ClusterFire()
+    {
+        int randShot = Random.Range(0, 2);
+
+        for(int i = 0; i <= clusterSize; i++)
+        {
+            float xChange = Random.Range((clusterVariation * -1f), clusterVariation);
+            float yChange = Random.Range((clusterVariation * -1f), clusterVariation);
+
+            float targetXChange = Random.Range((clusterMissVariation * -1f), clusterMissVariation);
+            float targetYChange = Random.Range((clusterMissVariation * -1f), clusterMissVariation);
+
+            var startPos = new Vector3(transform.position.x + xChange, transform.position.y + yChange, transform.position.z);
+
+            var targetPos = new Vector3(player.transform.position.x + targetXChange, player.transform.position.y + targetYChange, player.transform.position.z);
+
+            var distance = new Vector3(targetPos.x - transform.position.x, targetPos.y - transform.position.y, 0f);
+
+            if (randShot == 0)
+                DoShot(ShotType.Small, startPos, distance, 0f);
+
+            if (randShot == 1)
+                DoShot(ShotType.Medium, startPos, distance, 0f);
+        }
+
+    }
+
+    void TargetFire()
+    {
+        float targetXChange = Random.Range((targetMissVariation * -1f), targetMissVariation);
+        float targetYChange = Random.Range((targetMissVariation * -1f), targetMissVariation);
+
+        var targetPos = new Vector3(player.transform.position.x + targetXChange, player.transform.position.y + targetYChange, player.transform.position.z);
+
+        var distance = new Vector3(targetPos.x - transform.position.x, targetPos.y - transform.position.y, 0f);
+
+        DoShot(ShotType.Medium, transform.position, distance, 0f);
+    }
+
+    void WaveBarrage()
+    {
+        for(int i = 0; i < waveCount; i++)
+        {
+
+        }
+    }
+
+    bool MinimumTimeElapsed()
+    {
+        var now = Time.realtimeSinceStartup;
+        var elapsed = now - lastAttackTime;
+
+        if (elapsed >= attackInterval)
+            return true;
+
+        return false;
+    }
+}
--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -27,8 +27,64 @@ public class Shot : MonoBehaviour
 	public void Shoot(Vector3 targetPosition)
 	{
 		var shotBody = gameObject.GetComponent<Rigidbody2D>();
-		shotBody.AddForce(targetPosition * speed, ForceMode2D.Force);
+
+        Debug.Log("before" + targetPosition);
+
+        Vector2 force = GetNormalizedForce(targetPosition);
+
+        Debug.Log("after" + force);
+
+        shotBody.velocity = new Vector2(force.x * speed, force.y * speed);
+
 	}
+
+    Vector2 GetNormalizedForce(Vector3 targetPosition)
+    {
+        float x = targetPosition.x;
+        float y = targetPosition.y;
+
+        if(Mathf.Abs(x) > 1 || Mathf.Abs(y) > 1)
+        {
+            if(Mathf.Abs(x) > Mathf.Abs(y))
+            {
+                var finalX = 1;
+
+                if (x < 0)
+                    finalX *= -1;
+
+                var finalY = Mathf.Abs(y) / Mathf.Abs(x);
+
+                if (y < 0)
+                    finalY *= -1;
+
+                x = finalX;
+                y = finalY;
+            }
+            
+            if(Mathf.Abs(x) < Mathf.Abs(y))
+            {
+                var finalY = 1;
+
+                if (y < 0)
+                    finalY *= -1;
+
+                var finalX = Mathf.Abs(x) / Mathf.Abs(y);
+
+                if (x < 0)
+                    finalX *= -1;
+
+                x = finalX;
+                y = finalY;
+            }
+
+            if(x == y)
+            {
+                //TODO
+            }
+        }
+
+        return new Vector2(x, y);
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
