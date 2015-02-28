d685e2691286db8f37290e4243afd3d8f3c3e7f2
maxarnheiter <maxarnheiter@gmail.com>
2015-02-28 17:06:12 -0500

    19. Intro Screen UI Progress
    Made some major progress on the intro screen UI and background
    animation. Also figured out what I want to do with the healthbars and
    their UI's.

---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star : MonoBehaviour 
+{
+
+    public float fallSpeed;
+
+    Rigidbody2D body;
+	
+	void Start () 
+    {
+        body = gameObject.GetComponent<Rigidbody2D>();
+
+        body.velocity = Vector3.down * fallSpeed;
+	}
+	
+	
+	void Update () 
+    {
+	
+	}
+}
---
+++ Assets/Scripts/StarSpawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StarSpawner : MonoBehaviour 
+{
+
+    public float spawnWidthRange;
+
+    public List<GameObject> stars;
+
+    public float spawnInterval;
+
+    float lastStarSpawnTime;
+    
+	
+	void Start () 
+    {
+	
+	}
+	
+	
+	void Update () 
+    {
+        var now = Time.realtimeSinceStartup;
+        var elapsed = now - lastStarSpawnTime;
+
+        if (elapsed >= spawnInterval)
+        {
+            SpawnStar();
+            lastStarSpawnTime = now;
+        }
+
+	}
+
+    void SpawnStar()
+    {
+        int randomStarIndex = Random.Range(0, stars.Count);
+        var randomStar = stars.ElementAt(randomStarIndex);
+
+        var randomWidth = Random.Range((spawnWidthRange * -1), spawnWidthRange);
+
+        var newStar = GameObject.Instantiate(randomStar, new Vector3(transform.position.x + randomWidth, transform.position.y, 0f), Quaternion.identity) as GameObject;
+
+        newStar.transform.parent = transform;
+    }
+}
