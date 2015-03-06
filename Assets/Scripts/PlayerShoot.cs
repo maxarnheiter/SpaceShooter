5c1c4ebfc7f38d6c51be66d637475e5ed4757d24
maxarnheiter <maxarnheiter@gmail.com>
2015-03-06 12:44:35 -0500

    27. Player Upgrades and Battle Sounds
    Finished the player shot upgrade modes (0-5), and found some more sounds
    for the actual battle scenes.

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -52,6 +52,18 @@ public class PlayerShoot : MonoBehaviour {
 			case 2:
 				Level_2_Shoot();
 			break;
+            case 3:
+                Level_3_Shoot();
+            break;
+            case 4:
+                Level_4_Shoot();
+            break;
+            case 5:
+                Level_5_Shoot();
+            break;
+            default:
+                Level_5_Shoot();
+            break;
 		}
 
 		lastShotTime = Time.realtimeSinceStartup;
@@ -114,6 +126,39 @@ public class PlayerShoot : MonoBehaviour {
 
 	void Level_3_Shoot()
 	{
-
+        DoShot(ShotType.Large, transform.position, Vector3.up, 1.5f);
 	}
+
+    void Level_4_Shoot()
+    {
+        var shot1XOffset = 0.25f;
+        var shot2XOffset = -0.25f;
+        var shot3YOffset = 0.15f;
+
+        var position1 = new Vector3(transform.position.x + shot1XOffset, transform.position.y, transform.position.z);
+        var position2 = new Vector3(transform.position.x + shot2XOffset, transform.position.y, transform.position.z);
+        var position3 = new Vector3(transform.position.x, transform.position.y + shot3YOffset, transform.position.z);
+
+        DoShot(ShotType.Small, position1, Vector3.up, 1.5f);
+        DoShot(ShotType.Small, position2, Vector3.up, 1.5f);
+        DoShot(ShotType.Large, position3, Vector3.up, 1.5f);
+    }
+
+    void Level_5_Shoot()
+    {
+        for(int i=0; i < 5; i++)
+        {
+            float randX = Random.Range(-0.25f, 0.25f);
+            float randSpeed = Random.Range(1.3f, 1.9f);
+
+            int randType = Random.Range(0, 2);
+
+            var startPos = new Vector3(transform.position.x + randX, transform.position.y, transform.position.z);
+
+            if (randType == 0)
+                DoShot(ShotType.Small, startPos, Vector3.up, randSpeed);
+            if (randType == 1)
+                DoShot(ShotType.Large, startPos, Vector3.up, randSpeed);
+        }
+    }
 }
